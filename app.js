846a949c15b9455fdbb24a070f59abd17ae855d6
Breyanna Vass <breyannavass@gmail.com>
2019-09-04 16:50:22 -0400

    Adding flash sessions and global variables

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const expressLayouts = require('express-ejs-layouts');
 const mongoose = require('mongoose');
+const flash = require('connect-flash');
+const session = require('express-session');
 
 const app = express();
 
@@ -18,6 +20,26 @@ app.set('view engine', 'ejs');
 
 // Body Parser
 app.use(express.urlencoded({ extended: false }));
+
+// Express Session
+app.use(
+    session({
+        secret: 'secret',
+        resave: true,
+        saveUninitialized: true
+    })
+);
+
+// Connect Flash
+app.use(flash());
+
+// Variables
+app.use(function (req, res, next) {
+    res.locals.success_msg = req.flash('success_msg');
+    res.locals.error_msg = req.flash('error_msg');
+    res.locals.error = req.flash('error');
+    next();
+});
 
 // Routes
 app.use('/', require('./routes/index'));
