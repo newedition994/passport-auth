1b5fb18d1fdcd46a2f11f6ab1b1cb1147708a140
Breyanna Vass <breyannavass@gmail.com>
2019-09-04 16:23:30 -0400

    adding validation errors

--- routes/users.js
+++ routes/users.js
@@ -27,6 +27,18 @@ router.post('/register', (req, res) => {
     if (password.length < 6) {
         errors.push({ msg: 'Password must be at least 6 characters' })
     }
+
+    if (errors.length > 0) {
+        res.render('register', {
+            errors,
+            name,
+            email,
+            password,
+            password2
+        })
+    } else {
+        res.send('pass');
+    }
 });
 
 module.exports = router;
