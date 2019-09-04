a5f8b3d8f2ecfb7a66e288ff7428f6671c026e08
Breyanna Vass <breyannavass@gmail.com>
2019-09-04 15:39:57 -0400

    rendering out the login and register pages

--- routes/users.js
+++ routes/users.js
@@ -2,9 +2,9 @@ const express = require('express');
 const router = express.Router();
 
 // Login page
-router.get('/login', (req, res) => res.send('Login'));
+router.get('/login', (req, res) => res.render('login'));
 
 // Sign Up page
-router.get('/register', (req, res) => res.send('Register'));
+router.get('/register', (req, res) => res.render('register'));
 
 module.exports = router;
